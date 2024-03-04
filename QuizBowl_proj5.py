826fccda147d80f349ab0b8cb829735d41fb0c5d
Abigail Pipkin <abigailpipkinttu@gmail.com>
2024-03-03 18:12:06 -0600

    edited quizbowl file separately and copy & pasted in updated one

--- QuizBowl_proj5.py
+++ QuizBowl_proj5.py
@@ -1,17 +1,39 @@
+import sqlite3
+
+conn = sqlite3.connect('Fridayproj5.db')
+cursor = conn.cursor()
+
+# Used GAI to help extract table name -- "QuestAns"
+#cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+#tables = cursor.fetchall()
+#print("Tables in the database:")
+#for table in tables:
+    #print(table[0])
+
+# Now will use GAI to help extract table column names -- "id" "question" "answer"
+#tableName = 'QuestAns'
+#cursor.execute("PRAGMA table_info({})".format(tableName))
+#columns = cursor.fetchall()
+#print("Columns in the table '{}'".format(tableName))
+#for column in columns:
+    #print(column[1])
+
+cursor.execute("SELECT question, answer from QuestAns")
+questionsList = cursor.fetchall()
+
 print("Welcome to The QuizBowl! Your questions start now.")
-questionsList = {"What is the tallest animal in the world? ":"Giraffe",
-                 "What is the anatomical term for the collarbone? ":"Clavicle",
-                 "What does the deepest point in the ocean measure (in meters)? ":"10,935",
-                 "When was the first computer invented? ":"1822",
-                 "What types of letters will cost you in Wheel of Fortune? ":"Vowels"}
+
 score = 0
-for question, answer in questionsList.items():
+for question, answer in questionsList:
     print(question)
     response = input("Your answer: ")
-    if response == answer:
+    if response.lower() == answer.lower():
         print("Correct!")
         score += 1
     else:
-        print("Incorrect.")
+        print("Incorrect. The answer is "+answer+".")
 
-print("Quiz ended. Your score is "+str(score)+" out of 5.")
+print("Quiz ended. Your score is "+str(score)+" out of "+ str(len(questionsList)) + ".")
+
+# GAI showed how to close the db connection - good habit to get into
+conn.close()
